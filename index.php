8b52fa152c13a6b951cfbfd7aa98387fd3e3aa95
marciopedrocomba <marciopedrocomba@gmail.com>
2020-04-15 04:29:35 +0100

    terminei de fazer o design mobile do usuario final

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A-OneBlog</title>
+    <meta name="description" content="este é um blog feito pela A1-Itel">
+    <meta name="author" content="A1-Itel">
+    <meta name="keywords" content="blog">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="wrapper">
+            <div class="navbar-nav">
+                <a href="index" class="logo">A-OneBlog</a>
+                <div class="sidebar-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <main class="main">
+        <section class="main-content">
+            <section class="sidebar">
+
+            </section>
+
+            <section class="blog-inner">
+                <div class="blog-inner-top">
+                    <a href="#">
+                        <div class="wrapper">
+                            <div class="blog-inner-top-main">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                </article>
+                                <img src="img/1.jpg" alt="">
+                            </div>
+                        </div>
+                    </a>
+                    <div class="blog-inner-top-latest">
+                        <div class="wrapper">
+                            <a href="#">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <span class="article-author">Márcio Comba</span>
+                                </article>
+                            </a>
+                            
+                            <a href="#">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <span class="article-author">Márcio Comba</span>
+                                </article>
+                            </a>
+
+                            <a href="#">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <span class="article-author">Márcio Comba</span>
+                                </article>
+                            </a>
+
+                            <a href="#">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <span class="article-author">Márcio Comba</span>
+                                </article>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="blog-inner-main-content">
+                    <h2>All News</h2>
+                    <div class="wrapper">
+                        
+                        <a href="#">
+                            <div class="blog-bigger">
+                                <img src="img/2.jpg" alt="">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/3.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/3.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/3.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/3.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/3.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/1.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/2.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-bigger">
+                                <img src="img/3.jpg" alt="">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/2.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/2.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-normal">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                                <article>
+                                    <img src="img/2.jpg" alt="">
+                                </article>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="blog-bigger">
+                                <img src="img/1.jpg" alt="">
+                                <article>
+                                    <h3 class="article-title">Vote-by-mail should be having its moment. will it?</h3>
+                                    <p class="article-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsa tenetur ea, saepe quidem quod iusto facere sint doloremque asperiores dolor debitis, atque porro dolorem aspernatur inventore provident deleniti unde?</p>
+                                    <span class="article-author">Márcio Comba</span>
+                                    <span class="article-date">11:13 pm WAT April 13. 2020</span>
+                                </article>
+                            </div>
+                        </a>
+                    </div>
+                    
+                    <button type="button" class="btn-load">Carregar Mais</button>
+                </div>
+
+            </section>
+
+        </section>
+    </main>
+
+    <footer class="footer">
+        <p>&copy;2020 A-OneBlog all rights reserved</p>
+    </footer>
+
+</body>
+</html>
